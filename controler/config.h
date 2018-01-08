e225e1e6c12d1041148cfa34530f80c6f3da9033
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-01-08 16:38:05 +0100

    blank config file for controler

---
+++ controler/config.h
@@ -0,0 +1,9 @@
+const char* ssid = "..."; // wifi SSID
+const char* password = "..."; // wifi password
+const char* OTApassword = "..."; // OTA pasword
+
+int resetPin = 16; // D0
+const char* server = "192.168.0.1"; // server adress
+const char* thermometer = "192.168.0.4"; // thermometer adress
+int update_time = 10000;
+float default_temp = 20.00;
