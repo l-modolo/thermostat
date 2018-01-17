cc14c869af5900a4ea5ff2a79a46a1c6c7440c5e
Laurent Modolo <laurent@modolo.fr>
2018-01-17 21:51:54 +0100

    add DHT22 to controler config

--- controler/config.h
+++ controler/config.h
@@ -3,7 +3,8 @@ const char* password = "..."; // wifi password
 const char* OTApassword = "..."; // OTA pasword
 
 int resetPin = 16; // D0
+int pinDHT22 = 5;  // D1
 const char* server = "192.168.0.1"; // server adress
 const char* thermometer = "192.168.0.4"; // thermometer adress
 int update_time = 10000;
-float default_temp = 20.00;
+float default_temp = 19.00;
